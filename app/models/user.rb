ab931a4ad885d9734f951cb7e715d19b746eaa59
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-12 12:51:25 +0900

    新規登録時のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
  
  #後ほどアソシエーションを組む
-         
-  validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
-  validates :birthday,        presence: true
+    validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}, length: { maximum: 6}
+  
+  with_options presence: true do
+
+    validates :nickname
+    validates :birthday
+    validates :last_name, format: {with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"}
+    validates :first_name, format: {with:/\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"}
+    validates :last_name_kana, format: {with:/\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"}
+    validates :first_name_kana, format: {with:/\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"}
+    
+  end
+  
 end
