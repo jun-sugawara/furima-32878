fe786e258961342bc3575d4e89632fa40a226501
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-14 19:46:46 +0900

    Gemfile記述場所の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき'
+     it "必須項目が全て入力されていれば登録できる"do
+      expect(@user).to be_valid
+    end
+  
+  end
 end
