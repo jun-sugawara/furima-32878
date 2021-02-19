25b401adcb60cc0d3526e3c93a0f578184c8e47d
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-19 11:38:52 +0900

    アプリケーションコントローラーのkyesから２箇所削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana,
+                                      keys: [:nickname, :first_name, :last_name, :first_name_kana,
                                              :last_name_kana, :birthday])
   end
 end
