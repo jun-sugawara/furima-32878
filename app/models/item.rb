32cc2ac25c23f3edcd529c587822602ac89212cd
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-19 20:57:04 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,22 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :fee_id
+    validates :from_id
+    validates :day_id
+    validates :price, length: { in: 300..9999999 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :state_id
+  belongs_to :fee_id
+  belongs_to :from_id
+  belongs_to :day_id
 end
