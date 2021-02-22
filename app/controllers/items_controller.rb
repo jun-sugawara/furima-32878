271de97f6f934ad9a84cb258ea8597e8527ed2f3
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-22 11:24:05 +0900

    コントローラー追記
    ・newアクションを追加
    ・createアクションを追加
    ・プライベートメソッド以下に参照カラムを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,15 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :state_id, :fee_id, :from_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
 end
