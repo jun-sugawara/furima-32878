f4b2c5bd74410417068a0e1ee617246ce80d45a4
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-02-22 15:28:05 +0900

    itemモデルの異常系テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,5 +10,64 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
+
+    context '商品出品がうまくいかないとき' do
+      it '商品画像が選択されていなければ出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明が空では出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーが選択されていないと出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+      it '商品の状態が選択されていないと出品できない' do
+        @item.state_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State Select")
+      end
+      it '配送料の負担が選択されていないと出品できない' do
+        @item.fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Fee Select")
+      end
+      it '発送元の地域が選択されていないと出品できない' do
+        @item.from_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("From Select")
+      end
+      it '発送までの日数が選択されていないと出品できない' do
+        @item.day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day Select")
+      end
+      it '販売価格が空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '販売価格は半角数字でなければ出品できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+      it '販売価格¥300~¥9,999,999の範囲でなければ出品できない' do
+        @item.price = '10'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+    end
+
   end
 end
