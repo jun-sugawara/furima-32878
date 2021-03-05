ded15a2eda2a2495811298435cb3988866e64a58
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-05 14:49:16 +0900

    購入情報が保存されるように実装（クレカ決済未実装）

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,9 +2,24 @@ class BuysController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    @item_buy = ItemBuy.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @item_buy = ItemBuy.new(buy_params) 
+    if @item_buy.valid?
+      @item_buy.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  
+  def buy_params
+    params.require(:item_buy).permit(:postal_code, :from_id, :municipality, :house_number, :building_name, :tell).merge(user_id: current_user.id, item_id: params[:item_id], buy_id: params[:buy_id])
   end
 
 end
