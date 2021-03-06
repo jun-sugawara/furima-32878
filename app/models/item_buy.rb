bb1190e6b1c9fa949148fe04cad843963c1c27d2
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-06 10:10:02 +0900

    item_buyモデル編集
    ・バリデーション設定
    ・トークンをattr_accexxorに追加

--- app/models/item_buy.rb
+++ app/models/item_buy.rb
@@ -1,8 +1,9 @@
 class ItemBuy
   include ActiveModel::Model
-  attr_accessor :postal_code, :from_id, :municipality, :house_number, :building_name, :tell, :user_id, :item_id, :buy_id
+  attr_accessor :postal_code, :from_id, :municipality, :house_number, :building_name, :tell, :user_id, :item_id, :buy_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
     validates :from_id, numericality: {other_than: 1, message: 'Select'}
     validates :municipality
@@ -12,8 +13,6 @@ class ItemBuy
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    binding.pry
     Address.create(postal_code: postal_code, from_id: from_id, municipality: municipality, house_number: house_number, tell: tell, buy_id: buy.id)
-    
   end
 end
