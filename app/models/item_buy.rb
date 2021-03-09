cc1e7c635f3a866a53837b2aca99d6de99baeb91
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-09 16:40:23 +0900

    電話番号のバリデーションに10桁も許可

--- app/models/item_buy.rb
+++ app/models/item_buy.rb
@@ -8,7 +8,7 @@ class ItemBuy
     validates :from_id, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :house_number
-    validates :tell, format: { with: /\A\d{11}\z/, message: 'Input only number' }
+    validates :tell, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
   end
